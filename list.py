be9c021ad89c9f9cd422036434d99b928cb61881
kirainl <49083078+kirainl@users.noreply.github.com>
2019-07-14 23:07:02 +0800

    注释

--- list.py
+++ list.py
@@ -3,6 +3,7 @@
 import os
 
 songs = ['first.mp3','fire.mp3', 'wn.mp3','firel.mp3','cron.mp3','anime1.mp3','anime2.mp3','anime3.mp3']
+# 这里改mp3文件名
 for song in songs:
    os.system('mpg123 -m -C -q -s /home/pi/music/'+song+' | sudo pifm - 106.3 44100')
 
